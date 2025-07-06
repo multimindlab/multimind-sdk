3da77e91cef58d9cc9510b4b05f3173159cd7289
Nikhil Kumar <nikhilkrishnakumar98@gmail.com>
2025-07-06 02:55:38 +0200

    patch2

--- multimind/prompts/__init__.py
+++ multimind/prompts/__init__.py
@@ -2,7 +2,7 @@
 Prompts module for managing and assembling prompts.
 """
 
-from .prompt_assembly import PromptAssembly, PromptConfig
+from .prompt_assembly import PromptAssembly, PromptAssemblyConfig as PromptConfig
 from .advanced_prompting import AdvancedPrompting, PromptType
 
 __all__ = [
